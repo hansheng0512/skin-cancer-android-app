9ea1ab1e2318dffde32f7e734b31684c22ab297a
Han Sheng <hanshengliang@outlook.com>
2022-02-05 20:06:40 +0800

    :sparkles: TFlite Integration

--- App/src/main/java/ai/certifai/imagecapture/ResultActivity.java
+++ App/src/main/java/ai/certifai/imagecapture/ResultActivity.java
@@ -16,12 +16,23 @@ package ai.certifai.imagecapture;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.label.Category;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+import ai.certifai.imagecapture.ml.ModelSkin;
 import ai.certifai.imagecapture.utils.ImageReader;
 
 /**
@@ -36,6 +47,8 @@ public class ResultActivity extends AppCompatActivity
 
     private String fileLoc;
 
+    private EditText label_result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -44,12 +57,43 @@ public class ResultActivity extends AppCompatActivity
 
         // define layouts
         image = findViewById(R.id.resultView);
+        label_result = findViewById(R.id.label_result);
 
         // load image
         File imageFile = getImageFile();
         Bitmap imageBitmap = ImageReader.readFileToImage(imageFile);
 
+        // Tflite Inference
+        try {
+            ModelSkin model = ModelSkin.newInstance(this);
 
+            // Creates inputs for reference.
+            TensorImage image = TensorImage.fromBitmap(imageBitmap);
+
+            // Runs model inference and gets result.
+            ModelSkin.Outputs outputs = model.process(image);
+            List<Category> probability = outputs.getProbabilityAsCategoryList();
+
+            double maxSoFar = 0;
+            String selectedLabel = "";
+
+            // for each loop
+            for (Category catDetails : probability)
+            {
+                if (catDetails.getScore() > maxSoFar)
+                {
+                    maxSoFar = Math.round(catDetails.getScore() * 100.0) / 100.0;
+                    selectedLabel = catDetails.getLabel();
+                }
+            }
+
+            label_result.setText("This is " + selectedLabel + " (" + maxSoFar * 100 + "%) ");
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
 
         setImage(imageBitmap);
     }
